9021443e7c1f8e5ce988265c97704b95aff6a8f0
projectgrup6 <116723888+projectgrup6@users.noreply.github.com>
2025-06-24 14:36:03 +0530

    Update login_page.py

--- webautomation/pages/login_page.py
+++ webautomation/pages/login_page.py
@@ -4,15 +4,15 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def login(driver, username, password):
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    wait = WebDriverWait(driver, timeout=30)
-    wait.until(EC.visibility_of_element_located((By.NAME, "username"))).send_keys(username)
+    wait = WebDriverWait(driver, timeout=40)
+    wait.until(EC.presence_of_element_located((By.NAME, "username"))).send_keys(username)
     driver.find_element(By.NAME, "password").send_keys(password)
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
 def is_login_successful(driver):
-    wait = WebDriverWait(driver, timeout=30)
-    return wait.until(EC.visibility_of_element_located((By.XPATH, "//ul[@class='oxd-main-menu']/li[8]/a")))
+    wait = WebDriverWait(driver, timeout=40)
+    return wait.until(EC.presence_of_element_located((By.XPATH, "//ul[@class='oxd-main-menu']/li[8]/a")))
 
 def is_login_failed(driver):
-    wait = WebDriverWait(driver, timeout=30)
-    return wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[1]/div/span")))
+    wait = WebDriverWait(driver, timeout=40)
+    return wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[1]/div/span")))
