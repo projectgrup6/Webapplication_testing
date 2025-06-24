753c993d2de7131673272a8a530a92e36653fd36
projectgrup6 <116723888+projectgrup6@users.noreply.github.com>
2025-06-24 14:44:40 +0530

    Update test_login.py

--- webautomation/tests/test_login.py
+++ webautomation/tests/test_login.py
@@ -5,7 +5,13 @@ def test_login_pass(driver):
     assert is_login_successful(driver).is_displayed()
     print("Login Passed")
 
-def test_login_failed(driver):
-    login(driver, "Admin", "admin23")
-    assert is_login_failed(driver).is_displayed()
-    print("Login Failed")
+def is_login_failed(driver):
+    wait = WebDriverWait(driver, timeout=20)
+    try:
+        return wait.until(
+            EC.visibility_of_element_located((By.XPATH, "//p[contains(text(),'Invalid credentials')]"))
+        )
+    except Exception as e:
+        print("❌ Could not find login error message.")
+        driver.save_screenshot("login_failed_debug.png")
+        raise e
