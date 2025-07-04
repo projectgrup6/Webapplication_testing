7fe02f5afe7c17b5a1f6aaba991620e03ef4de0f
projectgrup6 <116723888+projectgrup6@users.noreply.github.com>
2025-07-04 16:49:11 +0530

    Update conftest.py

--- webautomation/conftest.py
+++ webautomation/conftest.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 import pytest
 
-edge_path = r'C:\Users\DELL\drivers\msedgedriver.exe'
+edge_path = r'C:\Users\DELL\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe'
 
 @pytest.fixture(scope="module")
 def driver():
     options = Options()
     services = Service(executable_path=edge_path)
-    driver = webdriver.Edge(options=options, service=services)
+    driver = webdriver.Chrome(options=options, service=services)
     yield driver
     driver.quit()
